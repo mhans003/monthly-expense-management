6ad032b60b38b89e2230460484f2dee198b6ebf5
mhans003 <michaeledwardhanson@gmail.com>
2020-11-16 20:50:21 -0800

    Fixed various styling issues. See notes in commit for more details.

--- public/js/budget.js
+++ public/js/budget.js
@@ -10,13 +10,32 @@ function balance() {
   outputResults(balance, totalExpense);
 }
 
-function outputResults(balance, totalExpense){
+function outputResults(balance, totalExpense) {
+  //Clear old results:
+  if (
+    document.getElementById("balanceDiv") &&
+    document.getElementById("totalExpenseDiv")
+  ) {
+    document
+      .getElementById("budgetFields")
+      .removeChild(document.getElementById("balanceDiv"));
+    document
+      .getElementById("budgetFields")
+      .removeChild(document.getElementById("totalExpenseDiv"));
+  }
+
   const balanceDiv = document.createElement("div");
+  balanceDiv.setAttribute("id", "balanceDiv");
   const totalExpenseDiv = document.createElement("div");
+  totalExpenseDiv.setAttribute("id", "totalExpenseDiv");
   //balanceDiv.classList.add("");
 
   const balanceSpan = document.createElement("span");
-  balanceSpan.classList.add("text-success");
+  if (balance < 1) {
+    balanceSpan.classList.add("text-danger");
+  } else {
+    balanceSpan.classList.add("text-success");
+  }
   balanceSpan.innerText = `$${balance.toFixed(2)}`;
 
   const expenseSpan = document.createElement("span");
